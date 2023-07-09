78f4708f748cbe6496ef045df974049800c57cec
Uriel MELIE <7274352-iconoyuri@users.noreply.gitlab.com>
2023-07-09 06:25:39 +0100

    create contribution api ok

--- routers/contribution.py
+++ routers/contribution.py
@@ -1,7 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException, status
 from schemas import Contribution, ContributionData
-from typing import List
 from fastapi_pagination import LimitOffsetPage, paginate
+import py2neo_schemas as nodes
+from functions import graph_driver
+from oauth2 import get_current_user
 
 router = APIRouter(
     prefix='/contribution',tags=['Contribution']
@@ -20,5 +22,25 @@ def get_contribution(id:str):
 
 
 @router.post('/')
-def post_contribution(contribution:ContributionData):
+def post_contribution(contribution:ContributionData, credentials=Depends(get_current_user)):
+    driver = graph_driver(credentials)
+    if contribution.montant <= 0:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Montant inférieur à 1")
+
+    createur = nodes.User.match(driver, contribution.email_user).first()
+    if not createur:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    fond = nodes.Fond.match(driver, contribution.id_fond).first()
+    if not fond:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    
+    caisse = list(fond.caisse)[0] if len(list(fond.caisse)) > 0 else None    
+    if not caisse:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="caisse non existante")
+    
+    # return createur.fonds_contribues.get(fond,'montant')
+    createur.fonds_contribues.add(fond, {'montant': contribution.montant + createur.fonds_contribues.get(fond,'montant')})
+    caisse.montant_courant = caisse.montant_courant + contribution.montant
+    driver.push(createur)
+    driver.push(caisse)
     ...
